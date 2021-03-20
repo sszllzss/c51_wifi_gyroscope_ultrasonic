be707e0dc5981b6853f656aa7811afd2da9b579d
sszlbg <sszllzss@foxmail.com>
2021-03-20 22:59:20 +0800

    添加外部中断驱动

--- device/exti.c
+++ device/exti.c
@@ -1,7 +1,7 @@
 
 
-
-#include	"exti.h"
+#include "gpio.h"
+#include "exti.h"
 static Ext_interrupt_cb_t xdata EXT0_interrupt_cb = NULL;
 static Ext_interrupt_cb_t xdata EXT1_interrupt_cb = NULL;
 static Ext_interrupt_cb_t xdata EXT2_interrupt_cb = NULL;
@@ -12,35 +12,35 @@ static Ext_interrupt_cb_t xdata EXT4_interrupt_cb = NULL;
 void Ext_INT0 (void) interrupt INT0_VECTOR		//进中断时已经清除标志
 {
 	if(EXT0_interrupt_cb)
-        EXT0_interrupt_cb(P32);
+        EXT0_interrupt_cb(EXT_INT0,P32);
 }
 
 /********************* INT1中断函数 *************************/
 void Ext_INT1 (void) interrupt INT1_VECTOR		//进中断时已经清除标志
 {
 	if(EXT1_interrupt_cb)
-        EXT1_interrupt_cb(P33);
+        EXT1_interrupt_cb(EXT_INT1,P33);
 }
 
 /********************* INT2中断函数 *************************/
 void Ext_INT2 (void) interrupt INT2_VECTOR	//
 {
 	if(EXT2_interrupt_cb)
-        EXT2_interrupt_cb(P36);
+        EXT2_interrupt_cb(EXT_INT2,P36);
 }
 
 /********************* INT3中断函数 *************************/
 void Ext_INT3 (void) interrupt INT3_VECTOR
 {
 	if(EXT3_interrupt_cb)
-        EXT3_interrupt_cb(P37);
+        EXT3_interrupt_cb(EXT_INT3,P37);
 }
 
 /********************* INT4中断函数 *************************/
 void Ext_INT4 (void) interrupt INT4_VECTOR
 {
 	if(EXT4_interrupt_cb)
-        EXT4_interrupt_cb(P30);
+        EXT4_interrupt_cb(EXT_INT4,P30);
 }
 
 //========================================================================
@@ -62,6 +62,8 @@ uint8_t	Ext_Inilize(uint8_t EXT, EXTI_InitTypeDef *INTx)
 		if(INTx->EXTI_Mode == EXT_MODE_Fall)	IT0 = 1;	//下降沿中断
 		else									IT0 = 0;	//上升,下降沿中断
         if(INTx->EXTI_Interrupt == ENABLE)		EXT0_interrupt_cb = INTx->interrupt_cb;	//回调
+        
+        GPIO_Inilize(GPIO_P3,GPIO_Pin_2, GPIO_HighZ);
 		return	0;		//成功
 	}
 
@@ -74,6 +76,7 @@ uint8_t	Ext_Inilize(uint8_t EXT, EXTI_InitTypeDef *INTx)
 		if(INTx->EXTI_Mode == EXT_MODE_Fall)	IT1 = 1;	//下降沿中断
 		else									IT1 = 0;	//上升,下降沿中断
         if(INTx->EXTI_Interrupt == ENABLE)		EXT1_interrupt_cb = INTx->interrupt_cb;	//回调
+        GPIO_Inilize(GPIO_P3,GPIO_Pin_3, GPIO_HighZ);
 		return	0;		//成功
 	}
 
@@ -82,6 +85,7 @@ uint8_t	Ext_Inilize(uint8_t EXT, EXTI_InitTypeDef *INTx)
 		if(INTx->EXTI_Interrupt == ENABLE)	INT_CLKO |=  (1 << 4);	//允许中断	
 		else								INT_CLKO &= ~(1 << 4);	//禁止中断
         if(INTx->EXTI_Interrupt == ENABLE)		EXT2_interrupt_cb = INTx->interrupt_cb;	//回调
+        GPIO_Inilize(GPIO_P3,GPIO_Pin_6, GPIO_HighZ);
 		return	0;		//成功
 	}
 
@@ -90,6 +94,7 @@ uint8_t	Ext_Inilize(uint8_t EXT, EXTI_InitTypeDef *INTx)
 		if(INTx->EXTI_Interrupt == ENABLE)	INT_CLKO |=  (1 << 5);	//允许中断	
 		else								INT_CLKO &= ~(1 << 5);	//禁止中断
         if(INTx->EXTI_Interrupt == ENABLE)		EXT3_interrupt_cb = INTx->interrupt_cb;	//回调
+        GPIO_Inilize(GPIO_P3,GPIO_Pin_7, GPIO_HighZ);
 		return	0;		//成功
 	}
 
@@ -98,6 +103,7 @@ uint8_t	Ext_Inilize(uint8_t EXT, EXTI_InitTypeDef *INTx)
 		if(INTx->EXTI_Interrupt == ENABLE)	INT_CLKO |=  (1 << 6);	//允许中断	
 		else								INT_CLKO &= ~(1 << 6);	//禁止中断
         if(INTx->EXTI_Interrupt == ENABLE)		EXT4_interrupt_cb = INTx->interrupt_cb;	//回调
+        GPIO_Inilize(GPIO_P3,GPIO_Pin_0, GPIO_HighZ);
 		return	0;		//成功
 	}
 	return 2;	//失败
--- device/exti.h
+++ device/exti.h
@@ -4,15 +4,15 @@
 
 #include	"config.h"
 
-#define	EXT_INT0			0	//初始化外中断0
-#define	EXT_INT1			1	//初始化外中断1
-#define	EXT_INT2			2	//初始化外中断2
-#define	EXT_INT3			3	//初始化外中断3
-#define	EXT_INT4			4	//初始化外中断4
+#define	EXT_INT0			0	//初始化外中断0 P32
+#define	EXT_INT1			1	//初始化外中断1 P33
+#define	EXT_INT2			2	//初始化外中断2 P36
+#define	EXT_INT3			3	//初始化外中断3 P37
+#define	EXT_INT4			4	//初始化外中断4 P30
 
 #define	EXT_MODE_RiseFall	0	//上升沿/下降沿中断
 #define	EXT_MODE_Fall		1	//下降沿中断
-typedef void((*Ext_interrupt_cb_t)(uint8_t PCA_id,uint8_t value));
+typedef void((*Ext_interrupt_cb_t)(uint8_t EXT,uint8_t value));
 typedef struct
 {
 	uint8_t	EXTI_Mode;			//中断模式,  	EXT_MODE_RiseFall, EXT_MODE_Fall
